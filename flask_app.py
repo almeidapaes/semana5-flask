a795420d1fc12e003cf2fd51bffba2f9e1b53b1f
almeidapaes <almeida.paes@aluno.ifsp.edu.br>
2026-08-17 22:16:54 -0300

    errata

--- flask_app.py
+++ flask_app.py
@@ -1,42 +1,22 @@
-from flask import Flask, render_template, request, make_response, redirect, abort
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
+from flask import Flask, render_template, session, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+app.config['SECRET_KEY'] = 'chave-secreta-semana05'
 
-@app.route('/')
-@app.route('/home')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
-@app.route('/user/<nome>/<prontuario>/<instituicao>')
-def user(nome, prontuario, instituicao):
-    return render_template('user.html', nome=nome, prontuario=prontuario, instituicao=instituicao)
-
-@app.route('/contextorequisicao')
-def contextorequisicao():
-    navegador = request.headers.get('User-Agent')
-    ip = request.remote_addr
-    host = request.host
-    return render_template('contexto.html', nome="Lucas Polli Crenitte", navegador=navegador, ip=ip, host=host)
-
-@app.route('/codigostatusdiferente')
-def codigostatusdiferente():
-    return 'Bad request', 400
-
-@app.route('/objetoresposta')
-def objetoresposta():
-    response = make_response('<h1>Este documento carrega um cookie!</h1>')
-    response.set_cookie('meu_cookie', 'valor_teste')
-    return response
-
-@app.route('/redirecionamento')
-def redirecionamento():
-    return redirect('https://www.ifsp.edu.br/')
-
-@app.route('/abortar')
-def abortar():
-    abort(404)
+if __name__ == '__main__':
+    app.run(debug=True)
